012b4995b127a145d63698f4f9b2e4339bf73449
yuuki-cloud <119059243+yuuki-cloud@users.noreply.github.com>
2024-12-05 14:49:22 +0900

    html追加

--- app.py
+++ app.py
@@ -16,5 +16,14 @@ for blueprint in blueprints:
 def index():
     return render_template('index.html')
 
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+@app.route('/user/touroku')
+def touroku():
+    return render_template('touroku.html')
+
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
+
