d353487773e902084ce799bcd8f11131a7558958
yuuki-cloud <119059243+yuuki-cloud@users.noreply.github.com>
2024-12-12 15:16:43 +0900

    データベースの登録変更

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
 from models import initialize_database
 from routes import blueprints
+import sqlite3
 
 app = Flask(__name__)
 
 # データベースの初期化
-initialize_database()
+# initialize_database()
+conn = sqlite3.connect('my_database.db')
+cursor = conn.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table;")
+
+tables = cursor.fetchall()
+if tables:
+    print('テーブルあります')
+else:
+    initialize_database()
+conn.close()
 
 # 各Blueprintをアプリケーションに登録
 for blueprint in blueprints:
@@ -16,14 +27,5 @@ for blueprint in blueprints:
 def index():
     return render_template('index.html')
 
-@app.route('/user')
-def user():
-    return render_template('user.html')
-
-@app.route('/user/touroku')
-def touroku():
-    return render_template('touroku.html')
-
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
-
