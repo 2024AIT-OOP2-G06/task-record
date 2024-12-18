b389090ed25b2be0540be5c77651d930095488a2
yuna <k23035kk@aitech.ac.jp>
2024-12-18 10:38:05 +0900

    棒グラフの追加, cssの調整
    ・task_chart.html (業務別平均体温グラフ) の追加
    ・体調アンケートに業務種の項目を追加
    ・index.htmlのcssの調整

--- models/physical.py
+++ models/physical.py
@@ -1,10 +1,12 @@
 from peewee import ForeignKeyField, DecimalField, BooleanField, CharField, Model
 from .db import db
 from .user import User
+from .task import Task
 
 class Physical(Model):
     user = ForeignKeyField(User, backref='physicals')
     temp = DecimalField()  # 固定小数点
+    task = ForeignKeyField(Task, backref='physicals')
     bad_good = BooleanField(default=True)
     bad_content = CharField()
     class Meta:
